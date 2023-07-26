20cab57c5f4db22690702cfb7373bafd6f6f6377
dasar <wanjikuestivah@gmail.com>
2023-07-26 19:48:56 +0300

    exercise from 1 to 7 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...args) {
+    // args.forEach((arg) => {
+    //     const index = array1.indexOf(arg);
+    //     if(index > -1)
+    //     array1.splice(index, 1);
+    // })
+    const newArray = []
+    array.forEach((item) => {
+     if (!args.includes(item)) {
+        newArray.push(item);
+     }   
+    });
+    return newArray;
 };
 
 // Do not edit below this line
